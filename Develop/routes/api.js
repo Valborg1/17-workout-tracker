9c73e3740d7e19be6780c058e143e6db3da819fd
Jared Lim <jaredlim1@gmail.com>
2021-01-22 22:05:21 -0700

    routes

---
+++ Develop/routes/api.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+  router.get("/api/workouts", function (req, res) {
+    Workout.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+  });
+
+  router.post("/api/workouts", function (req, res) {
+    Workout.create({})
+        .then(data => res.json(data))
+        .catch(err => {
+            console.log("err", err)
+            res.json(err)
+        })
+  });
+
+  router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true })
+        .then(data => res.json(data))
+        .catch(err => {
+            console.log("err", err)
+            res.json(err)
+        })
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
@@ -8,15 +10,16 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('Develop/public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./Develop/routes/api.js"));
+app.use(require("./Develop/routes/html-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
